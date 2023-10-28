8d7babf40b8fa0534a6202b7494ab9a67726d7f1
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-10-29 02:04:18 +0300

    b

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom";
-
+import { useSelector } from "react-redux";
 import { UserMenu } from "./UserMenu";
 
 
 export const AppBar = () => {
+    const isLoggedIn = useSelector(state => state.user.isLoggedIn)
     return <header style={{display: 'flex', justifyContent: 'space-between', padding: 8, borderBottom: '1px solid black', margin: 12}}>
                 <nav>
                     <Link to="/login">Log in</Link>
                 </nav>
-                <UserMenu />
+                {isLoggedIn && <UserMenu />}
+                
     </header>
 }
--- src/components/UserMenu.jsx
+++ src/components/UserMenu.jsx
@@ -1,6 +1,13 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { logOut } from "redux/UserSlice";
 
 export const UserMenu = () => {
+   const dispatch = useDispatch();
     const login = useSelector(state => state.user.login)
-    return <div>{login}<button>Log out</button></div>
+    return (
+        <div>
+            {login}
+            <button type="button" onClick={() => dispatch(logOut())}>Log out</button>
+        </div>
+    );
 }
