cd29677cf7326b1a0f8868ae9c6352c1622e0809
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-10-29 02:37:30 +0300

    Update DashboardPage.jsx

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,3 +1,17 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import { useSelector } from "react-redux";
+
 export const DashboardPage = () => {
+
+    const isLoggedIn = useSelector(state => state.user.isLoggedIn)
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(!isLoggedIn) {
+            navigate('/login', { replace: true })
+        }
+    })
+
     return <div>Dashboard Page</div>
 };
