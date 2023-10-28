fbb1154e11654627bf7a6a4727f3110059a60984
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-10-28 23:55:01 +0300

    Create LoginPages.jsx

---
+++ src/pages/LoginPages.jsx
@@ -0,0 +1,9 @@
+export const LoginPage = () => {
+    return <div>
+        <form>
+            <input type="text" name="login" />
+            <button type="submit">Log in</button>
+        </form>
+
+    </div>
+}
